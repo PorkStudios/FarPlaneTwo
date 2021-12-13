31788df5f6f17f35ed5b8f449ad6113e0517f83d
DaPorkchop_ <daporkchop@daporkchop.net>
2021-12-13 10:39:06 +0100

    move native code infrastructure into dedicated directory

--- natives/project.mk
+++ natives/project.mk
@@ -3,11 +3,14 @@ export SHELL		:=	/usr/bin/env bash
 export NPROC		:=	$(shell nproc)
 
 export ROOT_DIR		:=	$(CURDIR)
-export NATIVE_DIR   :=  $(ROOT_DIR)/src/main/native
+export NATIVE_DIR   :=  $(ROOT_DIR)/natives
 export TOOLCHAIN_DIR	:=	$(NATIVE_DIR)/toolchain
 export LIBS_DIR		:=	$(NATIVE_DIR)/lib
 export COMPILE_DIR  :=  $(ROOT_DIR)/build/native
 export OUTPUT_DIR   :=  $(ROOT_DIR)/src/main/resources/net/daporkchop/fp2
+
+export OUTPUT_SRC_PATH	:=	src/main/native/
+export OUTPUT_DST_PATH	:=	src/main/resources/net/daporkchop/fp2/
 
 export CXXFLAGS		:=	-O2 -ffast-math -std=c++17 -Wno-attributes
 export LDFLAGS		:=	$(CXXFLAGS) -shared
@@ -17,14 +20,14 @@ export HEADERFILES	:=	$(wildcard $(NATIVE_DIR)/*.h) $(wildcard $(NATIVE_DIR)/fp2
 export INCLUDES		:=	$(NATIVE_DIR) $(JAVA_HOME)/include $(JAVA_HOME)/include/linux
 
 export ARCHS		:=	x86_64-linux-gnu x86_64-w64-mingw32
-export MODULES_STD	:=  compat/x86 client/gl/func/c
-export MODULES_SIMD	:=  compat/cwg/noise compat/vanilla/biome/layer/c
+export MODULES_STD	:=  src/main/native/compat/x86 src/main/native/client/gl/func/c
+export MODULES_SIMD	:=  src/main/native/compat/cwg/noise src/main/native/compat/vanilla/biome/layer/c
 
 export LIBS			:=  vectorclass-2.01.03
 export LIB_TASKS	:=  $(addprefix $(LIBS_DIR)/,$(addsuffix .dl,$(LIBS)))
 
-export WIN_LIB_DIR	:=	$(OUTPUT_DIR)/compat/windows/
-export WIN_LIBS		:=	$(addprefix $(WIN_LIB_DIR), libgcc_s_seh-1.dll libwinpthread-1.dll libstdc++-6.dll)
+export WIN_LIB_DIR	:=	$(OUTPUT_DIR)/compat/windows
+export WIN_LIBS		:=	$(addprefix $(WIN_LIB_DIR)/,libgcc_s_seh-1.dll libwinpthread-1.dll libstdc++-6.dll)
 
 .PHONY: build clean .FORCE
 
@@ -34,14 +37,16 @@ build: $(foreach arch,$(ARCHS),$(arch).arch) $(LIB_TASKS) $(WIN_LIBS)
 	@echo "built all libraries for $*"
 
 %.mod_std: .FORCE $(LIB_TASKS)
-	@$(MAKE) --no-print-directory -C $(NATIVE_DIR) -f $(NATIVE_DIR)/module.mk ARCH=$(subst /,,$(dir $*)) BASE_ARCH=$(word 1,$(subst -, ,$(subst /,,$(dir $*)))) MODULE=$(subst _,/,$(notdir $*)) build
+	@$(MAKE) --no-print-directory -C $(ROOT_DIR) -f $(NATIVE_DIR)/module.mk ARCH=$(subst /,,$(dir $*)) BASE_ARCH=$(word 1,$(subst -, ,$(subst /,,$(dir $*)))) MODULE=$(subst _,/,$(notdir $*)) build
 
 %.mod_simd: .FORCE $(LIB_TASKS)
-	@$(MAKE) --no-print-directory -C $(NATIVE_DIR) -f $(NATIVE_DIR)/simd.mk ARCH=$(subst /,,$(dir $*)) BASE_ARCH=$(word 1,$(subst -, ,$(subst /,,$(dir $*)))) MODULE=$(subst _,/,$(notdir $*)) build
+	@$(MAKE) --no-print-directory -C $(ROOT_DIR) -f $(NATIVE_DIR)/simd.mk ARCH=$(subst /,,$(dir $*)) BASE_ARCH=$(word 1,$(subst -, ,$(subst /,,$(dir $*)))) MODULE=$(subst _,/,$(notdir $*)) build
 
-clean:
-	@[ ! -d $(COMPILE_DIR) ] || rm -r $(COMPILE_DIR)
-	@for f in $(MODULES_STD) $(MODULES_SIMD); do [ ! -d $(OUTPUT_DIR)/$$f ] || rm -r $(OUTPUT_DIR)/$$f; done
+clean: $(foreach module,$(subst $(OUTPUT_SRC_PATH),$(OUTPUT_DST_PATH),$(MODULES_STD) $(MODULES_SIMD)),$(ROOT_DIR)/$(module).clean) $(foreach dir,$(COMPILE_DIR) $(WIN_LIB_DIR),$(dir).clean)
+
+%.clean: .FORCE
+	@echo "deleting $*"
+	@[ ! -d $* ] || rm -r $*
 
 %.dl:
 	@[ -d $(LIBS_DIR) ] || mkdir -p $(LIBS_DIR)
@@ -50,7 +55,7 @@ clean:
 	@mv $(basename $(notdir $@))/ $(LIBS_DIR)
 	@touch $@
 
-$(WIN_LIB_DIR)%:
+$(WIN_LIB_DIR)/%:
 	@[ -d $(WIN_LIB_DIR) ] || mkdir -p $(WIN_LIB_DIR)
 	@echo "copying $*"
 	@cp `x86_64-w64-mingw32-g++ -print-file-name=$*` $@
--- natives/simd.mk
+++ natives/simd.mk
@@ -6,4 +6,4 @@ build: $(foreach ext,$(SIMD_EXTENSIONS),$(ext).simd_ext)
 	@echo "built all simd targets for $(MODULE) on $(ARCH) ($(SIMD_EXTENSIONS))"
 
 %.simd_ext: .FORCE
-	@$(MAKE) --no-print-directory -C $(NATIVE_DIR) -f $(NATIVE_DIR)/module.mk SIMD_EXT=$* build
+	@$(MAKE) --no-print-directory -f $(NATIVE_DIR)/module.mk SIMD_EXT=$* build
