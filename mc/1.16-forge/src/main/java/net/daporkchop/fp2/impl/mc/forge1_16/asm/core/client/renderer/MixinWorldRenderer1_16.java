ba1ea41d8c3cc06a42af8dd56cca3041e49ef7ea
DaPorkchop_ <daporkchop@daporkchop.net>
2022-02-19 17:27:26 +0100

    actually call fp2 rendering code

---
+++ mc/1.16-forge/src/main/java/net/daporkchop/fp2/impl/mc/forge1_16/asm/core/client/renderer/MixinWorldRenderer1_16.java
@@ -0,0 +1,122 @@
+/*
+ * Adapted from The MIT License (MIT)
+ *
+ * Copyright (c) 2020-2022 DaPorkchop_
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation
+ * files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy,
+ * modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software
+ * is furnished to do so, subject to the following conditions:
+ *
+ * Any persons and/or organizations using this software must include the above copyright notice and this permission notice,
+ * provide sufficient credit to the original authors of the project (IE: DaPorkchop_), as well as provide a link to the original project.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+ * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
+ * BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ *
+ */
+
+package net.daporkchop.fp2.impl.mc.forge1_16.asm.core.client.renderer;
+
+import com.mojang.blaze3d.matrix.MatrixStack;
+import net.daporkchop.fp2.core.client.IFrustum;
+import net.daporkchop.fp2.core.mode.api.client.IFarRenderer;
+import net.daporkchop.fp2.core.mode.api.ctx.IFarClientContext;
+import net.minecraft.client.renderer.ActiveRenderInfo;
+import net.minecraft.client.renderer.RenderType;
+import net.minecraft.client.renderer.WorldRenderer;
+import net.minecraft.client.renderer.culling.ClippingHelper;
+import net.minecraft.client.world.ClientWorld;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import static net.daporkchop.fp2.core.FP2Core.*;
+import static net.daporkchop.lib.common.util.PorkUtil.*;
+
+/**
+ * @author DaPorkchop_
+ */
+@Mixin(WorldRenderer.class)
+public abstract class MixinWorldRenderer1_16 {
+    @Shadow
+    private ClientWorld level;
+
+    @Inject(method = "Lnet/minecraft/client/renderer/WorldRenderer;setupRender(Lnet/minecraft/client/renderer/ActiveRenderInfo;Lnet/minecraft/client/renderer/culling/ClippingHelper;ZIZ)V",
+            at = @At("HEAD"),
+            require = 1)
+    private void fp2_setupRender_prepare(ActiveRenderInfo info, ClippingHelper clippingHelper, boolean useCapturedFrustum, int frameCount, boolean playerSpectator, CallbackInfo ci) {
+        fp2().client().currentPlayer().ifPresent(player -> {
+            IFarClientContext<?, ?> context = player.fp2_IFarPlayerClient_activeContext();
+            IFarRenderer renderer;
+            if (context != null && (renderer = context.renderer()) != null) {
+                //TODO: update renderability tracker
+                // this.fp2_vanillaRenderabilityTracker.update(uncheckedCast(this));
+
+                this.level.getProfiler().push("fp2_prepare");
+                renderer.prepare((IFrustum) clippingHelper); //TODO: this doesn't work
+                this.level.getProfiler().pop();
+            }
+        });
+    }
+
+    @Unique
+    private int fp2_toLayerIndex(RenderType type) {
+        if (type == RenderType.solid()) {
+            return IFarRenderer.LAYER_SOLID;
+        } else if (type == RenderType.cutout()) {
+            return IFarRenderer.LAYER_CUTOUT;
+        } else if (type == RenderType.translucent()) {
+            return IFarRenderer.LAYER_TRANSPARENT;
+        } else {
+            return -1;
+        }
+    }
+
+    @Inject(method = "Lnet/minecraft/client/renderer/WorldRenderer;renderChunkLayer(Lnet/minecraft/client/renderer/RenderType;Lcom/mojang/blaze3d/matrix/MatrixStack;DDD)V",
+            at = @At(value = "INVOKE",
+                    target = "Lnet/minecraft/client/renderer/RenderType;setupRenderState()V",
+                    shift = At.Shift.AFTER),
+            require = 1, allow = 1)
+    private void fp2_renderChunkLayer_pre(RenderType type, MatrixStack matrixStack, double cameraX, double cameraY, double cameraZ, CallbackInfo ci) {
+        int layer = this.fp2_toLayerIndex(type);
+        if (layer < 0) {
+            return;
+        }
+
+        fp2().client().currentPlayer().ifPresent(player -> {
+            IFarClientContext<?, ?> context = player.fp2_IFarPlayerClient_activeContext();
+            IFarRenderer renderer;
+            if (context != null && (renderer = context.renderer()) != null) {
+                this.level.getProfiler().push("fp2_render_pre");
+                renderer.render(layer, true);
+                this.level.getProfiler().pop();
+            }
+        });
+    }
+
+    @Inject(method = "Lnet/minecraft/client/renderer/WorldRenderer;renderChunkLayer(Lnet/minecraft/client/renderer/RenderType;Lcom/mojang/blaze3d/matrix/MatrixStack;DDD)V",
+            at = @At("RETURN"),
+            require = 1, allow = 2)
+    private void fp2_renderChunkLayer_post(RenderType type, MatrixStack matrixStack, double cameraX, double cameraY, double cameraZ, CallbackInfo ci) {
+        int layer = this.fp2_toLayerIndex(type);
+        if (layer < 0) {
+            return;
+        }
+
+        fp2().client().currentPlayer().ifPresent(player -> {
+            IFarClientContext<?, ?> context = player.fp2_IFarPlayerClient_activeContext();
+            IFarRenderer renderer;
+            if (context != null && (renderer = context.renderer()) != null) {
+                this.level.getProfiler().push("fp2_render_post");
+                renderer.render(layer, false);
+                this.level.getProfiler().pop();
+            }
+        });
+    }
+}
