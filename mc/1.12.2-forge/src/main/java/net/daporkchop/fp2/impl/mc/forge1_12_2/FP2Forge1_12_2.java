6febc6f56361842155d9d1384e904209b7fc8f26
DaPorkchop_ <daporkchop@daporkchop.net>
2022-07-13 15:30:04 +0200

    core: bump forgerocks dependency version to 6.28.2
    this fixes some build issues, and is also a long-due change anyway

--- mc/1.12.2-forge/src/main/java/net/daporkchop/fp2/impl/mc/forge1_12_2/FP2Forge1_12_2.java
+++ mc/1.12.2-forge/src/main/java/net/daporkchop/fp2/impl/mc/forge1_12_2/FP2Forge1_12_2.java
@@ -1,7 +1,7 @@
 /*
  * Adapted from The MIT License (MIT)
  *
- * Copyright (c) 2020-2021 DaPorkchop_
+ * Copyright (c) 2020-2022 DaPorkchop_
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation
  * files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy,
@@ -15,7 +15,6 @@
  * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
  * BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
- *
  */
 
 package net.daporkchop.fp2.impl.mc.forge1_12_2;
@@ -67,7 +66,7 @@ import static net.daporkchop.fp2.core.debug.FP2Debug.*;
  */
 @Mod(modid = FP2.MODID,
         useMetadata = true,
-        dependencies = "required-after:forgerocks@[6.20.3-1.12.2,);after:cubicchunks@[1.12.2-0.0.1188.0,)",
+        dependencies = "required-after:forgerocks@[6.28.2-1.12.2,);after:cubicchunks@[1.12.2-0.0.1188.0,)",
         acceptedMinecraftVersions = "1.12.2")
 public class FP2Forge1_12_2 extends FP2Core implements ResourceProvider {
     private FP2Client1_12_2 client;
