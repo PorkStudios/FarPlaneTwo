cce8484399106c9ab2eff0eb43da59b990eb52ec
DaPorkchop_ <daporkchop@daporkchop.net>
2022-07-14 13:50:49 +0200

    core: don't add tiles at invalid positions to the renderability index

--- core/src/main/java/net/daporkchop/fp2/core/mode/common/client/BakeManager.java
+++ core/src/main/java/net/daporkchop/fp2/core/mode/common/client/BakeManager.java
@@ -1,7 +1,7 @@
 /*
  * Adapted from The MIT License (MIT)
  *
- * Copyright (c) 2020-2021 DaPorkchop_
+ * Copyright (c) 2020-2022 DaPorkchop_
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation
  * files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy,
@@ -15,7 +15,6 @@
  * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
  * BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
- *
  */
 
 package net.daporkchop.fp2.core.mode.common.client;
@@ -191,11 +190,12 @@ public class BakeManager<POS extends IFarPos, T extends IFarTile> extends Abstra
     }
 
     protected void checkSelfRenderable(@NonNull POS pos) {
-        this.updateRenderable(pos,
-                this.coordLimits.contains(pos)
-                && this.tileCache.getTileCached(pos) != null
-                && (pos.level() == 0 || PorkUtil.<Stream<POS>>uncheckedCast(pos.down().allPositionsInBB(1, 3))
-                        .anyMatch(p -> this.coordLimits.contains(p) && this.tileCache.getTileCached(p) == null)));
+        if (this.coordLimits.contains(pos)) {
+            this.updateRenderable(pos,
+                    this.tileCache.getTileCached(pos) != null
+                    && (pos.level() == 0 || PorkUtil.<Stream<POS>>uncheckedCast(pos.down().allPositionsInBB(1, 3))
+                            .anyMatch(p -> this.coordLimits.contains(p) && this.tileCache.getTileCached(p) == null)));
+        }
     }
 
     protected void updateData(@NonNull POS pos, @NonNull Optional<IBakeOutput> optionalBakeOutput) {
