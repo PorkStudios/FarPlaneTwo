3c796ce095ad7204bd25a52520b9cff31b8a4082
SolDev69 <40839581+SolDev69@users.noreply.github.com>
2022-12-15 16:50:46 -0500

    can also get world size

--- src/main/java/net/daporkchop/fp2/asm/core/server/IMixinWorldBorder.java
+++ src/main/java/net/daporkchop/fp2/asm/core/server/IMixinWorldBorder.java
@@ -8,4 +8,7 @@ import org.spongepowered.asm.mixin.gen.Accessor;
 public interface IMixinWorldBorder {
     @Accessor("startDiameter")
     double getWorldBorderStartDiameter();
+
+    @Accessor("worldSize")
+    int getWorldSize();
 }
