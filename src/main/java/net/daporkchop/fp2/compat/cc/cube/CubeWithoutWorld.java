031ac67bd197842bf14dcdfc83c7c6b70c2fbe23
DaPorkchop_ <daporkchop@daporkchop.net>
2023-07-14 19:13:46 +0200

    fix build against newer versions of Cubic Chunks with 1.15-style 3d biomes

--- src/main/java/net/daporkchop/fp2/compat/cc/cube/CubeWithoutWorld.java
+++ src/main/java/net/daporkchop/fp2/compat/cc/cube/CubeWithoutWorld.java
@@ -1,7 +1,7 @@
 /*
  * Adapted from The MIT License (MIT)
  *
- * Copyright (c) 2020-2021 DaPorkchop_
+ * Copyright (c) 2020-2023 DaPorkchop_
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation
  * files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy,
@@ -15,7 +15,6 @@
  * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
  * BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
- *
  */
 
 package net.daporkchop.fp2.compat.cc.cube;
@@ -219,7 +218,7 @@ public class CubeWithoutWorld implements ICube {
     }
 
     @Override
-    public void setBiome(int localBiomeX, int localBiomeZ, Biome biome) {
+    public void setBiome(int localBiomeX, int localBiomeY, int localBiomeZ, Biome biome) {
         throw new UnsupportedOperationException();
     }
 
