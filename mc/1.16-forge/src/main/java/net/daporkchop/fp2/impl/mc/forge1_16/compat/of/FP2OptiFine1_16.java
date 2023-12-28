cd351f9d6b68e7c574712fefc4b9eada4fd8198b
DaPorkchop_ <daporkchop@daporkchop.net>
2023-12-28 17:40:56 +0100

    mc:1.16-forge: mark OptiFine compatibility mod as client-only

--- mc/1.16-forge/src/main/java/net/daporkchop/fp2/impl/mc/forge1_16/compat/of/FP2OptiFine1_16.java
+++ mc/1.16-forge/src/main/java/net/daporkchop/fp2/impl/mc/forge1_16/compat/of/FP2OptiFine1_16.java
@@ -1,7 +1,7 @@
 /*
  * Adapted from The MIT License (MIT)
  *
- * Copyright (c) 2020-2022 DaPorkchop_
+ * Copyright (c) 2020-2023 DaPorkchop_
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation
  * files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy,
@@ -15,18 +15,23 @@
  * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
  * BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
- *
  */
 
 package net.daporkchop.fp2.impl.mc.forge1_16.compat.of;
 
 import net.daporkchop.fp2.api.FP2;
 import net.daporkchop.fp2.api.event.FEventBus;
+import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.ExtensionPoint;
 import net.minecraftforge.fml.InterModComms;
+import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLConstructModEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.fml.loading.FMLEnvironment;
+import net.minecraftforge.fml.network.FMLNetworkConstants;
+import org.apache.commons.lang3.tuple.Pair;
 import org.apache.logging.log4j.LogManager;
 
 import java.util.function.Consumer;
@@ -44,6 +49,14 @@ public class FP2OptiFine1_16 {
 
     @SubscribeEvent
     public void construct(FMLConstructModEvent event) {
+        //accept all remote versions (we don't care whether or not this is installed on the remote side)
+        ModLoadingContext.get().registerExtensionPoint(ExtensionPoint.DISPLAYTEST, () -> Pair.of(() -> FMLNetworkConstants.IGNORESERVERONLY, (a, b) -> true));
+
+        if (FMLEnvironment.dist != Dist.CLIENT) {
+            //do nothing on the server side
+            return;
+        }
+
         if (!OFHelper1_16.OF) {
             LogManager.getLogger("fp2_optifine").info("OptiFine not present, integration will not be enabled!");
             return;
